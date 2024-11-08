cf87f93ce869774ee9d4eac491b4573f7a307f00
CY <iamCristYE@Gmail.com>
2024-11-08 19:56:05 +0800

    Test

---
+++ kkbox.py
@@ -0,0 +1,82 @@
+import requests
+
+
+import time
+import os
+from telegram import Bot
+
+
+# 发送压缩文件到Telegram
+async def send_file_to_telegram():
+    telegram_token = os.environ["bot_token"]  # 替换为你的Telegram bot token
+    telegram_chat_id = os.environ["chat_id"]  # 替换为你的频道或群组ID
+    bot = Bot(token=telegram_token)
+    archive_path = os.path.join(".", "kkbox.txt")
+    await bot.send_document(chat_id=telegram_chat_id, document=open(archive_path, "rb"))
+
+
+def send_telegram_message(token, channel_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": channel_id,  # Channel ID with "@" (e.g., "@your_channel_id")
+        "text": message,
+    }
+    response = requests.post(url, data=payload)
+    return response.json()
+
+
+async def main():
+    for code in range(275846980, 275856980, 1):
+        # Define the URL of the image
+        url = f"https://i.kfs.io/album/global/{code},1v1/fit/500x500.jpg"
+        # https://i.kfs.io/artist/global/407071,0v36/fit/500x500.jpg
+        print(code, url)
+        # Send a GET request to the URL
+
+        # Retry mechanism in case of connection issues
+        while True:
+            try:
+                response = requests.get(url)
+                last_modified = response.headers.get("last-modified")
+
+                time.sleep(1)
+                # Check if the request was successful (status code 200)
+                if response.status_code == 200:
+                    if len(response.content) > 6 * 1024:
+                        print(last_modified)
+                        # Save the content to a file
+                        # with open(f"img/{code}.jpg", "wb") as file:
+                        #     file.write(response.content)
+                        with open("kkbox.txt", "a") as f:
+                            f.write(url + "\t" + last_modified + "\n")
+                        send_telegram_message(
+                            os.environ["bot_token"],
+                            os.environ["chat_id"],
+                            url,
+                        )
+                        print("Image saved successfully.")
+                    else:
+                        with open("kkbox.txt", "a") as f:
+                            f.write(url + "\n")
+                        print("File < 6KB")
+                    break
+                else:
+                    print(
+                        "Failed to retrieve the image. Status code:",
+                        response.status_code,
+                    )
+                    break
+
+            except Exception as e:
+                print(e)
+                print("Connection lost. Retrying in 10 seconds...")
+                time.sleep(10)  # Wait 10 seconds before retrying
+
+        if code % 100 == 0:
+            await send_file_to_telegram()
+
+
+import asyncio
+
+if __name__ == "__main__":
+    asyncio.run(main())
