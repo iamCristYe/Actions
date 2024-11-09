ec3c5382ad52228472cc7ff81fb19dd82b6b0762
CY <iamCristYE@Gmail.com>
2024-11-09 08:42:17 +0800

    Test

--- kkbox.py
+++ kkbox.py
@@ -8,10 +8,11 @@ from telegram import Bot
 
 # 发送压缩文件到Telegram
 async def send_file_to_telegram():
+    end = int(os.environ["end"])
     telegram_token = os.environ["bot_token"]  # 替换为你的Telegram bot token
     telegram_chat_id = os.environ["chat_id"]  # 替换为你的频道或群组ID
     bot = Bot(token=telegram_token)
-    archive_path = os.path.join(".", "kkbox.txt")
+    archive_path = os.path.join(".", f"kkbox-{end}.txt")
     await bot.send_document(chat_id=telegram_chat_id, document=open(archive_path, "rb"))
 
 
@@ -27,7 +28,8 @@ def send_telegram_message(token, channel_id, message):
 
 async def main():
     start = int(os.environ["start"])
-    for code in range(start, start + 5000, 1):
+    end = int(os.environ["end"])
+    for code in range(start, end, 1):
         # Define the URL of the image
         url = f"https://i.kfs.io/album/global/{code},1v1/fit/500x500.jpg"
         # https://i.kfs.io/artist/global/407071,0v36/fit/500x500.jpg
@@ -48,7 +50,7 @@ async def main():
                         # Save the content to a file
                         # with open(f"img/{code}.jpg", "wb") as file:
                         #     file.write(response.content)
-                        with open("kkbox.txt", "a") as f:
+                        with open(f"kkbox-{end}.txt", "a") as f:
                             f.write(url + "\t" + last_modified + "\n")
                         send_telegram_message(
                             os.environ["bot_token"],
@@ -57,7 +59,7 @@ async def main():
                         )
                         print("Image saved successfully.")
                     else:
-                        with open("kkbox.txt", "a") as f:
+                        with open(f"kkbox-{end}.txt", "a") as f:
                             f.write(url + "\n")
                         print("File < 6KB")
                     break
@@ -73,8 +75,14 @@ async def main():
                 print("Connection lost. Retrying in 10 seconds...")
                 time.sleep(10)  # Wait 10 seconds before retrying
 
-        if code % 100 == 0:
-            await send_file_to_telegram()
+        if code % 1000 == 0:
+            while True:
+                try:
+                    await send_file_to_telegram()
+                    break
+                except:
+                    print("Connection lost. Retrying in 10 seconds...")
+                    time.sleep(10)  # Wait 10 seconds before retrying
 
 
 import asyncio
